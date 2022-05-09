7a4b39f6b33d77b8c8fe65419513a28641c8cc39
sary <arch.selmelegy@gmail.com>
2022-05-09 15:16:08 +0200

    Landing page photo creation

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,12 +3,10 @@ import "./landing.scss";
 const Landing = () => {
   return (
     <div className="Landing">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quod odio,
-      commodi incidunt sequi reprehenderit omnis natus accusantium voluptatibus
-      ducimus cumque sint excepturi enim vel dolor quibusdam ullam, animi
-      temporibus distinctio sit eaque mollitia dicta esse pariatur? Recusandae
-      alias molestias modi consectetur illo labore, iure, et a quos quasi
-      temporibus?
+      <div className="left"></div>
+      <div className="right">
+        <img src="./assets/villa-1.jpeg" alt="villa cover" />
+      </div>
     </div>
   );
 };
