2d9efb79bf9650b0ebfc30618458584381980791
sary <arch.selmelegy@gmail.com>
2022-05-09 15:03:14 +0200

    Navbar fixed position

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,16 @@
 import "./landing.scss";
 
 const Landing = () => {
-  return <div className="Landing">Landing</div>;
+  return (
+    <div className="Landing">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quod odio,
+      commodi incidunt sequi reprehenderit omnis natus accusantium voluptatibus
+      ducimus cumque sint excepturi enim vel dolor quibusdam ullam, animi
+      temporibus distinctio sit eaque mollitia dicta esse pariatur? Recusandae
+      alias molestias modi consectetur illo labore, iure, et a quos quasi
+      temporibus?
+    </div>
+  );
 };
 
 export default Landing;
