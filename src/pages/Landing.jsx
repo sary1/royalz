3aa7c6e5b398ad24d8e4ba50b1d077e72a8035e4
sary <arch.selmelegy@gmail.com>
2022-05-10 12:22:19 +0200

    Top search and logo nav

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,17 @@ import "./landing.scss";
 const Landing = () => {
   return (
     <div className="Landing">
-      <div className="left"></div>
+      <div className="left">
+        <div className="topNav">
+          <div className="logo">
+            <img src="./assets/logo.png" alt="logo" />
+          </div>
+          <div className="search">
+            <i class="search-icon fa-solid fa-magnifying-glass"></i>
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+      </div>
       <div className="right">
         <img src="./assets/villa-1.jpeg" alt="villa cover" />
       </div>
