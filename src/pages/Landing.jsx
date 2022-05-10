ddfb5dd7b62167aa5874e78890f28af9adbd12c2
sary <arch.selmelegy@gmail.com>
2022-05-10 12:43:24 +0200

    Landing page body contents

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,6 +13,27 @@ const Landing = () => {
             <input type="text" placeholder="Search" />
           </div>
         </div>
+        <div className="contents">
+          <span>Real Estate experts are here</span>
+          <h1>
+            Specialists in <i>development and property</i> management
+          </h1>
+          <div className="breakline"></div>
+          <div className="paragraphs">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Obcaecati, quo itaque quam atque dicta mollitia distinctio animi,
+              odio nostrum alias velit ipsum quod excepturi ipsam quia corrupti.
+            </p>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Obcaecati, quo itaque quam atque dicta mollitia distinctio animi,
+              odio nostrum alias velit ipsum quod excepturi ipsam quia corrupti.
+            </p>
+          </div>
+          <button>Read More</button>
+        </div>
       </div>
       <div className="right">
         <img src="./assets/villa-1.jpeg" alt="villa cover" />
